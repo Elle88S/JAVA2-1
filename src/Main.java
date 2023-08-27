3a94ffe6bbbaa18df985af3c08261427204bd8db
Elle <elle.sokolova92@gmail.com>
2023-08-27 19:00:42 +0300

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int mount = 15000;
+        int mile = 20;
+        int bonus = mount / mile;
+        System.out.println (bonus);
+    }
+
+}
